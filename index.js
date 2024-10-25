ca5fc1f0ad0020f2ec7d477b59ee1c4e5c3096bf
zh4r <zh4r@proton.me>
2024-10-25 23:21:38 +0300

    Responsive; Eye-candy; Accordion animation;

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 let accordion = document.querySelectorAll(".accordionBtn");
 
-for (let i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener("click", function () {
-        this.classList.toggle("active");
+accordion.forEach((btn, index) => {
+    btn.addEventListener("click", function() {
         let panel = this.nextElementSibling;
         let img = this.children[1];
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-            img.src = "./assets/images/icon-plus.svg"
+
+        if (panel.classList.contains("opened")) {
+            panel.classList.remove("opened");
+            img.src = "./assets/images/icon-plus.svg";
         } else {
-            panel.style.display = "block";
-            img.src = "./assets/images/icon-minus.svg"
+            panel.classList.add("opened");
+            img.src = "./assets/images/icon-minus.svg";
         }
     });
-};
+
+    if (index === 0) {
+        btn.nextElementSibling.classList.add("opened");
+        btn.children[1].src = "./assets/images/icon-minus.svg";
+    }
+});
